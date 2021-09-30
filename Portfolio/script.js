6c70f0393afcaa813d675f0921f6b4751d59d701
evelina <evety.1dtvd@gmail.com>
2021-09-30 23:01:08 +0300

    Fourth Stage Of Project - Add popup window on button More Information

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,16 +1,23 @@
 function openForm() {
     document.getElementById('myForm').style.display = 'block';
+    document.getElementById("body").classList.add("active");
 }
 
 function closeForm() {
     document.getElementById('myForm').style.display = 'none';
+    document.getElementById("body").classList.remove("active");
 
 }
+
+
 function openForm2() {
     document.getElementById('myForm2').style.display = 'block';
+    document.getElementById("myForm2").classList.add("active");
+    document.getElementById("overlay2").classList.add("active");
 }
-
 function closeForm2() {
     document.getElementById('myForm2').style.display = 'none';
+    document.getElementById("myForm2").classList.remove("active");
+    document.getElementById("overlay2").classList.remove("active");
 
 }
