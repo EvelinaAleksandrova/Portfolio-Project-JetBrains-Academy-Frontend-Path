b0094beec24a63fc28edd9868b5fdf002da1adc1
evelina <evety.1dtvd@gmail.com>
2021-10-05 15:36:39 +0300

    Fifth Stage Of Project - Add Hamburger menu

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,3 +1,6 @@
+let flagForOpen;
+
+
 function openForm() {
     document.getElementById('myForm').style.display = 'block';
     document.getElementById("body").classList.add("active");
@@ -12,11 +15,29 @@ function closeForm() {
 function openForm2() {
     document.getElementById('myForm2').style.display = 'block';
     document.getElementById("myForm2").classList.add("active");
-    document.getElementById("overlay2").classList.add("active");
 }
 function closeForm2() {
     document.getElementById('myForm2').style.display = 'none';
     document.getElementById("myForm2").classList.remove("active");
-    document.getElementById("overlay2").classList.remove("active");
+
 
 }
+function closeHamburger(){
+    document.getElementById('menu-hamburger').style.display = 'hidden';
+    document.getElementById("hamburger").style.height = "50px";
+    document.getElementById('name').style.top = "140px";
+
+}
+function openMenu(){
+    flagForOpen = true;
+    document.getElementById('menu-hamburger').style.visibility = 'visible';
+    document.getElementById("hamburger").style.height = "355px";
+    document.getElementById("menu-hamburger").style.textAlign = "center";
+    document.getElementById("menu-hamburger").style.padding = "25px";
+    document.getElementById('menuImg').style.alignItems = "center";
+    document.getElementById("menu-hamburger").style.fontSize = "20px";
+    document.getElementById('name').style.top = "450px";
+
+}
+
+
