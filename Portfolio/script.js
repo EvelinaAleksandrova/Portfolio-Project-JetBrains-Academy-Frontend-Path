864f73ad7b67e41731b861e4adb6a93c703bf07a
evelina <evety.1dtvd@gmail.com>
2021-10-05 15:52:49 +0300

    Fifth Stage Of Project - Add more functionality to hamburger menu

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,4 +1,4 @@
-let flagForOpen;
+let flagForOpen = true;
 
 
 function openForm() {
@@ -16,28 +16,31 @@ function openForm2() {
     document.getElementById('myForm2').style.display = 'block';
     document.getElementById("myForm2").classList.add("active");
 }
+
 function closeForm2() {
     document.getElementById('myForm2').style.display = 'none';
     document.getElementById("myForm2").classList.remove("active");
 
 
 }
-function closeHamburger(){
-    document.getElementById('menu-hamburger').style.display = 'hidden';
-    document.getElementById("hamburger").style.height = "50px";
-    document.getElementById('name').style.top = "140px";
 
-}
-function openMenu(){
-    flagForOpen = true;
-    document.getElementById('menu-hamburger').style.visibility = 'visible';
-    document.getElementById("hamburger").style.height = "355px";
-    document.getElementById("menu-hamburger").style.textAlign = "center";
-    document.getElementById("menu-hamburger").style.padding = "25px";
-    document.getElementById('menuImg').style.alignItems = "center";
-    document.getElementById("menu-hamburger").style.fontSize = "20px";
-    document.getElementById('name').style.top = "450px";
-
+function menuOpenAndClose() {
+    if (flagForOpen === true) {
+        document.getElementById('menu-hamburger').style.visibility = 'visible';
+        document.getElementById("hamburger").style.height = "355px";
+        document.getElementById("menu-hamburger").style.textAlign = "center";
+        document.getElementById("menu-hamburger").style.padding = "25px";
+        document.getElementById('menuImg').style.alignItems = "center";
+        document.getElementById("menu-hamburger").style.fontSize = "20px";
+        document.getElementById('name').style.top = "450px";
+        flagForOpen = false;
+    }
+    else{
+        document.getElementById('menu-hamburger').style.display = 'hidden';
+        document.getElementById("hamburger").style.height = "50px";
+        document.getElementById('name').style.top = "140px";
+        flagForOpen = true;
+    }
 }
 
 
